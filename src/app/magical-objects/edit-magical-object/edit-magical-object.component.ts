a2c79338c92927c0222a454f5056ea930d38b811
István Rózsa <istvan.rozsa@outlook.com>
2025-02-17 20:59:22 +0100

    Add MagicalObjectService integration for data fetching
    Integrated the MagicalObjectService into EditMagicalObjectComponent and MagicalObjectDetailsComponent to fetch magical object data by ID. Updated the components to handle and display retrieved data, and adapted ID parsing to ensure numeric type consistency.

--- src/app/magical-objects/edit-magical-object/edit-magical-object.component.ts
+++ src/app/magical-objects/edit-magical-object/edit-magical-object.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {
+  MagicalObjectService,
+  MagicalObjectViewModel,
+} from '../../core/services/magical-object.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-magical-object',
@@ -8,16 +13,31 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './edit-magical-object.component.scss',
 })
 export class EditMagicalObjectComponent implements OnInit {
-  magicalObjectId!: string;
+  magicalObjectId: number | undefined;
+  magicalObject: MagicalObjectViewModel | null = null;
+  editForm: FormGroup = new FormGroup({});
 
-  constructor(private readonly route: ActivatedRoute) {}
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly service: MagicalObjectService,
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
       if (id) {
-        this.magicalObjectId = id;
+        this.magicalObjectId = Number.parseInt(id);
         console.log('Magical Object ID:', this.magicalObjectId);
+
+        this.service.getById(this.magicalObjectId).subscribe({
+          next: (response) => {
+            this.magicalObject = response as MagicalObjectViewModel;
+            this.editForm.patchValue(this.magicalObject);
+          },
+          error: (error) => {
+            console.error('Error fetching magical object data:', error);
+          },
+        });
       }
     });
   }
--- src/app/magical-objects/magical-object-details/magical-object-details.component.ts
+++ src/app/magical-objects/magical-object-details/magical-object-details.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {
+  MagicalObjectService,
+  MagicalObjectViewModel,
+} from '../../core/services/magical-object.service';
 
 @Component({
   selector: 'app-magical-object-details',
@@ -8,16 +12,29 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './magical-object-details.component.scss',
 })
 export class MagicalObjectDetailsComponent implements OnInit {
-  magicalObjectId!: string;
+  magicalObjectId: number | undefined;
+  magicalObject: MagicalObjectViewModel | null = null;
 
-  constructor(private readonly route: ActivatedRoute) {}
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly service: MagicalObjectService,
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
       if (id) {
-        this.magicalObjectId = id;
+        this.magicalObjectId = Number.parseInt(id);
         console.log('Magical Object ID:', this.magicalObjectId);
+
+        this.service.getById(this.magicalObjectId).subscribe({
+          next: (response) => {
+            this.magicalObject = response as MagicalObjectViewModel;
+          },
+          error: (error) => {
+            console.error('Error fetching magical object data:', error);
+          },
+        });
       }
     });
   }
