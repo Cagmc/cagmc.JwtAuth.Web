92c8968f1c076069a9c5084d4c332ee63a8f3d9e
István Rózsa <istvan.rozsa@outlook.com>
2025-02-27 19:49:11 +0100

    Add logout test to login.spec.ts
    This commit introduces a new test for the logout functionality, ensuring users are redirected to the homepage after logging out. Removed an unnecessary timeout from the login test for better efficiency.

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,11 +9,23 @@ test('login', async ({ page }) => {
   await page.fill('input[type=text]', 'admin@cagmc.com');
   await page.fill('input[type=password]', '<PASSWORD>');
 
-  // Wait a second
-  await page.waitForTimeout(1000);
-
   // Submit
   await page.click('button[type=submit]');
 
   await expect(page).toHaveURL('/home');
+});
+
+test('logout', async ({ page }) => {
+  await page.goto('/login');
+
+  // Set email
+  await page.fill('input[type=text]', 'admin@cagmc.com');
+  await page.fill('input[type=password]', '<PASSWORD>');
+
+  // Submit
+  await page.click('button[type=submit]');
+
+  await page.click('a:has-text("Logout")');
+
+  await expect(page).toHaveURL('/');
 });
